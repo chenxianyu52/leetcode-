25167e7b6ab9185f3af0a9681fab5a2e8ab70875
chenxianyu52 <380286681@qq.com>
2022-04-25 19:02:07 +0800

    443-压缩字符串

---
+++ 字符串/数字与字符串间转换/443-压缩字符串/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int compress(char[] chars) {
+        int len = chars.length;
+        if (len <= 1) {
+            return len;
+        }
+        int j = 1;
+        char pre = chars[0];
+        int count = 1;
+        for (int i = 1; i < len; i++) {
+            if (chars[i] == pre) {
+                count++;
+            } else {
+                if (count == 1) {
+                    chars[j] = chars[i];
+                    j++;
+                } else {
+                    String countString = String.valueOf(count);
+                    char[] charsCount = countString.toCharArray();
+                    for (char c : charsCount) {
+                        chars[j] = c;
+                        j++;
+                    }
+                    chars[j] = chars[i];
+                    j++;
+                }
+                pre = chars[i];
+                count = 1;
+            }
+            System.out.println("i: " + i + "chars[i]" + chars[i] + "count " + count + " pre : " + pre);
+        }
+        System.out.println(count);
+        if (count > 1) {
+            String countString = String.valueOf(count);
+            char[] charsCount = countString.toCharArray();
+            for (char c : charsCount) {
+                chars[j] = c;
+                j++;
+            }
+        }
+        return j;
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:36 ms,击败了5.09% 的Java用户
+ * 内存消耗:41.6 MB,击败了5.22% 的Java用户
+ */
