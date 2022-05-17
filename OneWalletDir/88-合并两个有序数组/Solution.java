919e09317dfbd8fa870b110318f44317f247cebe
chenxianyu52 <380286681@qq.com>
2022-05-17 13:28:59 +0800

    88-合并两个有序数组

---
+++ OneWalletDir/88-合并两个有序数组/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 || j >= 0) {
+            if (i >= 0 && j >= 0) {
+                if (nums1[i] >= nums2[j]) {
+                    nums1[k] = nums1[i];
+                    i--;
+                } else {
+                    nums1[k] = nums2[j];
+                    j--;
+                }
+                k--;
+            } else if (i >= 0) {
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:41.1 MB,击败了74.90% 的Java用户
+ */
