98e62a6892d0664e614405abab74a73446ad8acd
chenxianyu52 <380286681@qq.com>
2022-07-21 09:34:19 +0800

    172-阶乘后的零

---
+++ 数与位/数字的位操作	/172-阶乘后的零/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += n / 5;
+            n /= 5;
+        }
+        return res;
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:38.2 MB,击败了95.91% 的Java用户
+ */
