3eecbed93f3b737fd26bee1c142939784861aa0c
chenxianyu52 <380286681@qq.com>
2022-07-26 11:36:01 +0800

    319-灯泡开关

---
+++ 数与位/数字的位操作	/319-灯泡开关/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:38.1 MB,击败了68.72% 的Java用户
+ */
