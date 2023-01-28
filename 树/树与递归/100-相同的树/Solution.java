b8721c3890dbd061aa889a8907b213215608df80
chenxianyu52 <380286681@qq.com>
2023-01-28 17:21:29 +0800

    100-相同的树

---
+++ 树/树与递归/100-相同的树/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:39.4 MB,击败了5.10% 的Java用户
+ */
