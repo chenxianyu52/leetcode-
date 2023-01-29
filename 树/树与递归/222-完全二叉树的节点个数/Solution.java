1fff47d03b0f682e65d3d976e5a5fec82f328b1d
chenxianyu52 <380286681@qq.com>
2023-01-29 10:32:11 +0800

    222-完全二叉树的节点个数

---
+++ 树/树与递归/222-完全二叉树的节点个数/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:44.3 MB,击败了35.50% 的Java用户
+ */
