f8ae2c369bafcbc072d710f0df7110ee0192e66b
chenxianyu52 <380286681@qq.com>
2023-01-29 10:58:11 +0800

    101-对称二叉树

---
+++ 树/树与递归/101-对称二叉树/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:39.9 MB,击败了27.54% 的Java用户
+ */
