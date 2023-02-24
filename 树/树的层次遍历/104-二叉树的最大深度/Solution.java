cead895eb9121642c75df547760aaa669db67c55
chenxianyu52 <380286681@qq.com>
2023-02-24 14:21:58 +0800

    104-二叉树的最大深度

---
+++ 树/树的层次遍历/104-二叉树的最大深度/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:41.1 MB,击败了80.22% 的Java用户
+ */
