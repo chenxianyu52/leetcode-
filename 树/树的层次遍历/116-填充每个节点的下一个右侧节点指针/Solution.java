d10534402988ccc58b8f40fed6752930cb6edadb
chenxianyu52 <380286681@qq.com>
2023-02-27 16:28:54 +0800

    116-填充每个节点的下一个右侧节点指针

---
+++ 树/树的层次遍历/116-填充每个节点的下一个右侧节点指针/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    Map<Integer, Node> map = new HashMap<>();
+
+    public Node connect(Node root) {
+        dfs(root, 0);
+        return root;
+    }
+
+    private void dfs(Node root, int depth) {
+        if (root == null) {
+            return;
+        }
+
+        if (map.containsKey(depth)) {
+            Node parent = map.get(depth);
+            parent.next = root;
+        }
+        map.put(depth, root);
+
+        dfs(root.left, depth + 1);
+        dfs(root.right, depth + 1);
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:1 ms,击败了71.84% 的Java用户
+ * 内存消耗:41.8 MB,击败了27.15% 的Java用户
+ */
