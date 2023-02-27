dbbe8945692a7a125c1952284804c6efac188e82
chenxianyu52 <380286681@qq.com>
2023-02-27 10:32:36 +0800

    112-路径总和

---
+++ 树/树的层次遍历/112-路径总和/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:41.5 MB,击败了34.12% 的Java用户
+ */
