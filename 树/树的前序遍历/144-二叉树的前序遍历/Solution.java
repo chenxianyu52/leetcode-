8ddc94d470498478442a471cbe1e7dce345a76b8
chenxianyu52 <380286681@qq.com>
2023-02-28 10:18:14 +0800

    144-二叉树的前序遍历

---
+++ 树/树的前序遍历/144-二叉树的前序遍历/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list);
+        return list;
+    }
+
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        dfs(root.left, list);
+        dfs(root.right, list);
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:39.3 MB,击败了96.79% 的Java用户
+ */
