acd237603b94f9625136cf4e148762716064abdd
chenxianyu52 <380286681@qq.com>
2023-09-04 16:29:50 +0800

    20230904-232-用栈实现队列

---
+++ 其他/20230904-232-用栈实现队列/Solution.java
@@ -0,0 +1,40 @@
+class MyQueue {
+    Stack<Integer> in = new Stack<>();
+    Stack<Integer> out = new Stack<>();
+
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.pop();
+    }
+
+    public int peek() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:39 MB,击败了92.02% 的Java用户
+ */
