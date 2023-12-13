63583f0fa46549a63bd10e2f5c683bc49e8d4b00
chenxianyu52 <380286681@qq.com>
2023-12-13 17:21:47 +0800

    63-不同路径II

---
+++ 动态规划/矩阵中的动态规划/63-不同路径II/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rowLen = obstacleGrid.length;
+        int colLen = obstacleGrid[0].length;
+        int[][] dp = new int[rowLen][colLen];
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                if (obstacleGrid[i][j] != 0) {
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if (i - 1 >= 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+        return dp[rowLen - 1][colLen - 1];
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:39.3 MB,击败了81.18% 的Java用户
+ */
