da874d8452ceede5b318a371e5ff437dafb3fb8c
chenxianyu52 <380286681@qq.com>
2024-01-19 11:54:24 +0800

    96-不同的二叉搜索树

---
+++ 动态规划/矩阵中的动态规划/96-不同的二叉搜索树/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
+/**
+ * 解答成功:
+ * 执行耗时:0 ms,击败了100.00% 的Java用户
+ * 内存消耗:39.4 MB,击败了6.96% 的Java用户
+ */
